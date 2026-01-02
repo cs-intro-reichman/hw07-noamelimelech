b0d3e8a016fd5b786c64741433cdeaced7943eb3
Noam Elimelech <noam.elimelech01@post.runi.ac.il>
2026-01-02 19:06:36 +0200

    Fix recursive base case

--- Binomial.java
+++ Binomial.java
@@ -1,23 +1,18 @@
 /** Computes the binomial(n,k) function. */
 public class Binomial { 
     public static void main(String[] args) {
-        //// Uncomment the version of binomial that you want to test
-        // Testing the basic binomial implementation:
         // System.out.println(binomial1(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
-        // Testing the optimized binomial implementation:
-        // System.out.println(binomial(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
+        System.out.println(binomial(Integer.parseInt(args[0]), Integer.parseInt(args[1])));
     }
     
-    // Computes the Binomial function, basic version.
-    public static int binomial1(int n, int k) { 
+    public static long binomial1(int n, int k) { 
         if (k > n) return 0;
         if (k == 0 || n == 0) return 1;
         return binomial1(n - 1, k) + binomial1(n - 1, k - 1);   
     }
     
-    // Computes the Binomial function, efficiently
-    public static int binomial(int n, int k) {
-        int[][] memo = new int[n + 1][k + 1];
+    public static long binomial(int n, int k) {
+        long[][] memo = new long[n + 1][k + 1];
         for (int i = 0; i <= n; i++) {
             for (int j = 0; j <= k; j++) {
                 memo[i][j] = -1;
@@ -26,7 +21,7 @@ public class Binomial {
         return binomial(n, k, memo);
     }
     
-    private static int binomial(int n, int k, int[][] memo) {
+    private static long binomial(int n, int k, long[][] memo) {
         if (memo[n][k] != -1) {
             return memo[n][k];
         }
